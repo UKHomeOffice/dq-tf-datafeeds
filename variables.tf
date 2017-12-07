f7eb77dbcb488d0af7a4ed75e585c5fd014da898
david-wirelab <info@wirelab.co.uk>
2017-12-07 11:50:27 +0000

    add rt association

--- variables.tf
+++ variables.tf
@@ -4,6 +4,7 @@ variable "data_pipe_apps_cidr_block" {}
 variable "data_feeds_cidr_block" {}
 variable "az" {}
 variable "name_prefix" {}
+variable "route_table_id" {}
 
 variable "df_postgres_ip" {
   description = "Mock IP address of database EC2 instance"
