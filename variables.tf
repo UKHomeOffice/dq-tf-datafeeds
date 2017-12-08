c7152f79eda888fdfe879911e933c4b909f70ef7
david-wirelab <info@wirelab.co.uk>
2017-12-08 08:40:32 +0000

    tag instances

--- variables.tf
+++ variables.tf
@@ -19,3 +19,18 @@ variable "df_web_ip" {
   description = "Mock IP address of web EC2 instance"
   default     = "10.1.4.21"
 }
+
+variable "service" {
+  default     = "dq-external-feed"
+  description = "As per naming standards in AWS-DQ-Network-Routing 0.5 document"
+}
+
+variable "environment" {
+  default     = "preprod"
+  description = "As per naming standards in AWS-DQ-Network-Routing 0.5 document"
+}
+
+variable "environment_group" {
+  default     = "dq-apps"
+  description = "As per naming standards in AWS-DQ-Network-Routing 0.5 document"
+}
