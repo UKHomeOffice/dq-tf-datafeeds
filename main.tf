8c593a25ce5b61b54b5d89a6985a30379157a070
nick-otter <nicholas.a.otter@gmail.com>
2018-01-08 15:11:18 +0000

    added naming suffix to main, deleted unused vars

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_subnet" "data_feeds" {
   availability_zone       = "${var.az}"
 
   tags {
-    Name = "${local.name_prefix}subnet"
+    Name = "subnet-${local.naming_suffix}"
   }
 }
 
@@ -26,10 +26,7 @@ module "df_postgres" {
   private_ip      = "${var.df_postgres_ip}"
 
   tags = {
-    Name             = "ec2-${var.service}-postgresql-${var.environment}"
-    Service          = "${var.service}"
-    Environment      = "${var.environment}"
-    EnvironmentGroup = "${var.environment_group}"
+    Name = "postgresql-${local.naming_suffix}"
   }
 }
 
@@ -41,10 +38,7 @@ module "df_web" {
   private_ip      = "${var.df_web_ip}"
 
   tags = {
-    Name             = "ec2-${var.service}-python-${var.environment}"
-    Service          = "${var.service}"
-    Environment      = "${var.environment}"
-    EnvironmentGroup = "${var.environment_group}"
+    Name = "python-${local.naming_suffix}"
   }
 }
 
@@ -52,7 +46,7 @@ resource "aws_security_group" "df_db" {
   vpc_id = "${var.appsvpc_id}"
 
   tags {
-    Name = "${local.name_prefix}db-sg"
+    Name = "sg-db-${local.naming_suffix}"
   }
 
   ingress {
@@ -83,7 +77,7 @@ resource "aws_security_group" "df_web" {
   vpc_id = "${var.appsvpc_id}"
 
   tags {
-    Name = "${local.name_prefix}web-sg"
+    Name = "sg-web-${local.naming_suffix}"
   }
 
   ingress {
--- variables.tf
+++ variables.tf
@@ -4,7 +4,6 @@ variable "data_pipe_apps_cidr_block" {}
 variable "data_feeds_cidr_block" {}
 variable "peering_cidr_block" {}
 variable "az" {}
-variable "name_prefix" {}
 
 variable "naming_suffix" {
   default     = false
@@ -24,19 +23,4 @@ variable "df_postgres_ip" {
 variable "df_web_ip" {
   description = "Mock IP address of web EC2 instance"
   default     = "10.1.4.21"
-}
-
-variable "service" {
-  default     = "dq-external-feed"
-  description = "As per naming standards in AWS-DQ-Network-Routing 0.5 document"
-}
-
-variable "environment" {
-  default     = "preprod"
-  description = "As per naming standards in AWS-DQ-Network-Routing 0.5 document"
-}
-
-variable "environment_group" {
-  default     = "dq-apps"
-  description = "As per naming standards in AWS-DQ-Network-Routing 0.5 document"
 }
