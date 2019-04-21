f6b39a048755c5bcd597711fd214f08cb368b0b4
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-04-21 20:03:17 +0100

    Renamed the vars to match apps

--- rds.tf
+++ rds.tf
@@ -55,8 +55,8 @@ resource "aws_security_group" "df_db" {
       "${var.opssubnet_cidr_block}",
       "${var.data_feeds_cidr_block}",
       "${var.peering_cidr_block}",
-      "${var.dq_lambda_subnet_cidr}",
-      "${var.dq_lambda_subnet_cidr_az2}",
+      "${var.lambda_subnet}",
+      "${var.lambda_subnet_az2}",
     ]
   }
 
